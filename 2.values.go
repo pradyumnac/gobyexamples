394c87734f00753bea9e6f99b4af0a1dab7712bc
pradyumna <pady92@gmail.com>
2020-05-30 17:30:55 +0530

    added two tutorials

---
+++ 2.values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("go" + "Lang")
+
+
+    fmt.Println("1+1" ,1+1)
+    fmt.Println("7/3 float :", 7.0/3.0)
+    fmt.Println("7/3 Int :", 7/3)
+    fmt.Println("7/3 mixed :", 7.0/3)
+    fmt.Println("7/3 mixed :", 7/3.0)
+
+    fmt.Println("And:", true && false)
+    fmt.Println("Or:", true ||false)
+    fmt.Println("Not:", !true)
+}
